9ca70d4ca55554882d764613b9ecb4e2cb5b878e
Liam <liambaldyga1@gmail.com>
2022-03-22 20:48:26 -0400

    Using ES6 classes

--- script.js
+++ script.js
@@ -28,11 +28,13 @@ form.addEventListener('submit', function (event) {
 addBook.onclick = popupModal;
 close.onclick = closeModal;
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
